a2d57a2fcc39744d3591c1dbb88f5b827c8bc5b7
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-01-10 12:36:48 -0600

    function

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const endPoint = "http://localhost:3000/api/v1/books"
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    getBooks()
+})
+    function getBooks() {
     fetch(endPoint);
     .then(response => response.json())
     .then(books => {
         console.log(books);
     })
-})
+}
