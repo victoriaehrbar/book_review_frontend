d8b08f42eb06037ad15fc0974d94793fd21469e3
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-01-11 11:49:51 -0600

    book.js updates

--- src/book.js
+++ src/book.js
@@ -1,1 +1,38 @@
-console.log("in book js");
+class Book {
+
+    constructor(book, bookAttributes) {
+      this.id = book.id
+      this.title = bookAttributes.title
+      this.author = syllabusAttributes.author
+      this.description = bookAttributes.description
+      this.category = syllabusAttributes.category
+      Book.all.push(this)
+      console.log(this);
+    }
+  
+    renderBookCard() {
+      return `
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            
+            <div class="card-body">
+              <h5 class="card-title">${this.title}</h5>
+              <h5 class="card-author">${this.author}</h5>
+              <p class="card-text">${this.description}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">Category: ${this.category.name}</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      `
+     
+    }
+  
+  }
+  
+  Book.all = [];
