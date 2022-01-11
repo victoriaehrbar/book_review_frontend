2680e9eb150d39eac52f3d96109af6e644eecba8
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-01-11 12:35:02 -0600

    updates

--- src/book.js
+++ src/book.js
@@ -19,12 +19,12 @@ class Book {
               <h5 class="card-title">${this.title}</h5>
               <h5 class="card-author">${this.author}</h5>
               <p class="card-text">${this.description}</p>
+              <small class="text-muted">Category: ${this.category.name}</small>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
-                <small class="text-muted">Category: ${this.category.name}</small>
               </div>
             </div>
           </div>
