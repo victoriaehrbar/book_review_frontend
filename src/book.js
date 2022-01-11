d7af0eacf7874e0630fed2b7639c79531c7b19f9
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-01-11 17:36:26 -0600

    book.js bookcard update

--- src/book.js
+++ src/book.js
@@ -10,27 +10,43 @@ class Book {
       console.log(this);
     }
   
-    renderBookCard() {
-      return `
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
+    // renderBookCard() {
+    //   return `
+    //     <div class="col-md-4">
+    //       <div class="card mb-4 shadow-sm">
             
-            <div class="card-body">
-              <h5 class="card-title">${this.title}</h5>
-              <h5 class="card-author">${this.author}</h5>
-              <p class="card-text">${this.description}</p>
-              <small class="text-muted">Category: ${this.category.name}</small>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      `
-     
+    //         <div class="card-body">
+    //           <h5 class="card-title">${this.title}</h5>
+    //           <h5 class="card-author">${this.author}</h5>
+    //           <p class="card-text">${this.description}</p>
+    //           <small class="text-muted">Category: ${this.category.name}</small>
+    //           <div class="d-flex justify-content-between align-items-center">
+    //             <div class="btn-group">
+    //               <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+    //               <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   `
+    
+    renderBookCard() {
+        let element = document.getElementById(`book-${this.id}`)
+        if(typeof(element) != 'undefined' && element != null){
+            return ""
+        } else { 
+            return `
+                <div id="book-${this.id}" data-id=${this.id}>
+                <ul class="list-group">
+                    <li class="list-group-item active"><strong>${this.title}</strong></li>
+                    <li class="list-group-item"><strong>Author:</strong> ${this.author}</li>
+                    <li class="list-group-item"><strong>Description:</strong> ${this.description}</li>
+                    <li class="list-group-item"><strong>Category:</strong> ${this.category}</li>
+                </ul>
+                </div>`
+            }
+
     }
   
   }
